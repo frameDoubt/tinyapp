240f1a403fa03ba1b2e9df870613643001c2a18b
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-20 03:56:17 +0000

    added random string function and handling of post requests from forms

--- express_server.js
+++ express_server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080;
 
+const generateRandomString = function() {
+  let alphNumArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  for (i = 0; i < 6; i++) {
+  let retStr = "";
+  retStr += alphNumArr[Math.floor(Math.random() * alphNumArr.length)];
+  return retStr;
+  };
+}
+
+
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouse.ca",
@@ -24,6 +37,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
